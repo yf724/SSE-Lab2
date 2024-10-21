acb9b8645228063f20307f2961d653dd2fcea0ed
Yiran Fan <yiran.fan24@imperial.ac.uk>
2024-10-21 16:59:47 +0100

    modified query4

--- src/app_test.py
+++ src/app_test.py
@@ -2,9 +2,9 @@ from app import process_query
 
 
 def test_knows_about_dinosaurs():
-    assert process_query("dinosaurs") == (
+    assert process_query() == (
             "Dinosaurs ruled the Earth 200 million years ago"
     )
 
-    def test_does_not_know_about_asteroids():
-        assert process_query("asteroids") == "Unknown"
+def test_does_not_know_about_asteroids():
+    assert process_query() == "Unknown"
