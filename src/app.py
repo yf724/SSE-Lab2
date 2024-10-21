6c78938836f9d527e511ec2d93f7dfb3e89146cc
Yiran Fan <yiran.fan24@imperial.ac.uk>
2024-10-21 16:30:26 +0100

    Updated requirements

--- src/app.py
+++ src/app.py
@@ -12,3 +12,10 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+
+def process_query(query):
+    if query == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    elif query == "asteroids":
+        return "Unknown"
