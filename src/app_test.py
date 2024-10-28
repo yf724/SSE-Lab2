a96633350fbac0c25e0420268c894e90c4880c28
Yiran Fan <yiran.fan24@imperial.ac.uk>
2024-10-28 16:38:21 +0000

    format

--- src/app_test.py
+++ src/app_test.py
@@ -47,9 +47,10 @@ def test_prime_number2():
             """Which of the following numbers are primes:
             37, 8, 73, 47, 25?""") == "37, 47, 73"
 
+
 def test_prime_number3():
     assert process_query(
-            """Which of the following numbers are primes: 
+            """Which of the following numbers are primes:
             28, 79, 8, 92, 48?""") == "79"
 
 
