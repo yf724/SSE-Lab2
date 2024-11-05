cc95807186acbc6577ee6580c8c4294ed1efa912
Yiran Fan <yiran.fan24@imperial.ac.uk>
2024-11-05 10:44:51 +0000

    added emoji api

--- src/app.py
+++ src/app.py
@@ -31,6 +31,7 @@ def github():
 def githubUsername():
     input_name = request.form.get("name")
     response = requests.get(f"https://api.github.com/users/{input_name}/repos")
+    emoji = response.get("https://github.githubassets.com/images/icons/emoji/unicode/1f947.png?v8")
     REPOS = []
     if response.status_code == 200:
         repos = response.json()
@@ -42,7 +43,7 @@ def githubUsername():
         commit = response_commits.json()
         commits = commit[0]['sha']
         REPOS.append({'repo': full_name, 'time': time, 'commits': commits})
-    return render_template("githubUsername.html", name=input_name, repos=REPOS)
+    return render_template("githubUsername.html", name=input_name, repos=REPOS, emoji=emoji)
 
 
 def process_query(query):
